3a55532f8f1792dfa2308b83b3183e79be28e308
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-17 16:06:20 -0500

    Add files via upload

---
+++ long_addition_1.py
@@ -0,0 +1,170 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Summations are found in python by using the addition operator (+), in which two addends on either side are summed.
+Basic usage is as follows:
+    print(5 + 3) 
+    sum = addendA + addendB
+    difference = positive + (-negative)
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed (works with v2 as well)
+    2. Navigate to a directory containing this file (long_addition_1.py)
+    3. Run the program with the following command: python3 long_addition_1.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+# Problem 1
+sum = 16 + 45 # variable sum created, is the sum of the two addends
+print("{0:<3} + {1:<3} = {2:<4}".format(16,45,sum)) # prints out the the two addends with the sum (formatted to be left aligned)
+
+# Problem 2
+sum = 13 + 40
+print("{0:<3} + {1:<3} = {2:<4}".format(13,40,sum))
+
+# Problem 3
+sum = 16 + 18
+print("{0:<3} + {1:<3} = {2:<4}".format(16,18,sum))
+
+# Problem 4
+sum = 77 + 19
+print("{0:<3} + {1:<3} = {2:<4}".format(77,19,sum))
+
+# Problem 5
+sum = 18 + 13
+print("{0:<3} + {1:<3} = {2:<4}".format(18,13,sum))
+
+# Problem 6
+sum = 41 + 15
+print("{0:<3} + {1:<3} = {2:<4}".format(41,15,sum))
+
+# Problem 7
+sum = 10 + 2
+print("{0:<3} + {1:<3} = {2:<4}".format(10,2,sum))
+
+# Problem 8
+sum = 18 + 18
+print("{0:<3} + {1:<3} = {2:<4}".format(18,18,sum))
+
+# Problem 9
+sum = 13 + 42
+print("{0:<3} + {1:<3} = {2:<4}".format(13,42,sum))
+
+# Problem 10
+sum = 15 + 5
+print("{0:<3} + {1:<3} = {2:<4}".format(15,5,sum))
+
+# Problem 11
+sum = 11 + 66
+print("{0:<3} + {1:<3} = {2:<4}".format(11,66,sum))
+
+# Problem 12
+sum = 11 + 55
+print("{0:<3} + {1:<3} = {2:<4}".format(11,55,sum))
+
+# Problem 13
+sum = 19 + 4
+print("{0:<3} + {1:<3} = {2:<4}".format(19,4,sum))
+
+# Problem 14
+sum = 166 + 3
+print("{0:<3} + {1:<3} = {2:<4}".format(166,3,sum))
+
+# Problem 15
+sum = 17 + 10
+print("{0:<3} + {1:<3} = {2:<4}".format(17,10,sum))
+
+# Problem 16
+sum = 80 + 41
+print("{0:<3} + {1:<3} = {2:<4}".format(80,41,sum))
+
+# Problem 17
+sum = 91 + 4
+print("{0:<3} + {1:<3} = {2:<4}".format(91,4,sum))
+
+# Problem 18
+sum = 119 + 40
+print("{0:<3} + {1:<3} = {2:<4}".format(119,40,sum))
+
+# Problem 19
+sum = 19 + 11
+print("{0:<3} + {1:<3} = {2:<4}".format(19,11,sum))
+
+# Problem 20
+sum = 76 + 71
+print("{0:<3} + {1:<3} = {2:<4}".format(76,71,sum))
+
+# Problem 21
+sum = 18 + 6
+print("{0:<3} + {1:<3} = {2:<4}".format(18,6,sum))
+
+# Problem 22
+sum = 91 + 18
+print("{0:<3} + {1:<3} = {2:<4}".format(91,18,sum))
+
+# Problem 23
+sum = 62 + 93
+print("{0:<3} + {1:<3} = {2:<4}".format(62,93,sum))
+
+# Problem 24
+sum = 60 + 64
+print("{0:<3} + {1:<3} = {2:<4}".format(60,64,sum))
+
+# Problem 25
+sum = 61 + 14
+print("{0:<3} + {1:<3} = {2:<4}".format(61,14,sum))
+
+# Problem 26
+sum = 100 + 21
+print("{0:<3} + {1:<3} = {2:<4}".format(100,21,sum))
+
+# Problem 27
+sum = 19 + 44
+print("{0:<3} + {1:<3} = {2:<4}".format(19,44,sum))
+
+# Problem 28
+sum = 61 + 16
+print("{0:<3} + {1:<3} = {2:<4}".format(61,16,sum))
+
+# Problem 29
+sum = 17 + 17
+print("{0:<3} + {1:<3} = {2:<4}".format(17,17,sum))
+
+# Problem 30
+sum = 18 + 150
+print("{0:<3} + {1:<3} = {2:<4}".format(18,150,sum))
+
+# Problem 31
+sum = 50 + 91
+print("{0:<3} + {1:<3} = {2:<4}".format(50,91,sum))
+
+# Problem 32
+sum = 100 + 86
+print("{0:<3} + {1:<3} = {2:<4}".format(100,86,sum))
+
+# Problem 33
+sum = 21 + 10
+print("{0:<3} + {1:<3} = {2:<4}".format(21,10,sum))
+
+# Problem 34
+sum = 49 + 75
+print("{0:<3} + {1:<3} = {2:<4}".format(49,75,sum))
+
+# Problem 35
+sum = 38 + 56
+print("{0:<3} + {1:<3} = {2:<4}".format(38,56,sum))
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_1/issues
+"""
